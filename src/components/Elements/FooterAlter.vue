8686e8795085c4d57ea894547082d85fc33a3120
Archimède Body <Archimedebody@rocketmail.com>
2020-12-17 16:19:40 +0100

    init

---
+++ src/components/Elements/FooterAlter.vue
@@ -0,0 +1,26 @@
+<template>
+  <!--START FOOTER ALTER-->
+  <div class="footer-alt">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="float-left pull-none">
+            <p class="copy-rights text-muted">{{ new Date().getFullYear() }} © Dorsin - X-Vue-Themes</p>
+          </div>
+          <div class="float-right pull-none">
+            <img src="images/payment.png" alt="payment-img" height="36" />
+          </div>
+          <div class="clearfix"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--START FOOTER ALTER-->
+</template>
+
+<script>
+export default {
+  name: "FooterAlter",
+  props: {}
+};
+</script>
---
+++ src/components/Sections/Social.vue
@@ -0,0 +1,46 @@
+<template>
+  <!--START SOCIAL-->
+  <section class="cta bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <ul class="list-inline social margin-t-20">
+            <li class="list-inline-item"
+              ><a href="" class="social-icon"><i class="mdi mdi-facebook"></i></a
+            ></li>
+            <li class="list-inline-item"
+              ><a href="" class="social-icon"><i class="mdi mdi-twitter"></i></a
+            ></li>
+            <li class="list-inline-item"
+              ><a href="" class="social-icon"><i class="mdi mdi-linkedin"></i></a
+            ></li>
+            <li class="list-inline-item"
+              ><a href="" class="social-icon"><i class="mdi mdi-google-plus"></i></a
+            ></li>
+            <li class="list-inline-item"
+              ><a href="" class="social-icon"><i class="mdi mdi-dribbble"></i></a
+            ></li>
+          </ul>
+        </div>
+        <div class="col-lg-3 margin-t-30">
+          <p class="margin-b-0 contact-title">
+            <i class="pe-7s-call"></i> &nbsp;<a href="tel:+243 81 20 44 091">+243 81 20 44 091</a>
+          </p>
+        </div>
+        <div class="col-lg-3 margin-t-30 text-right">
+          <p class="contact-title">
+            <i class="pe-7s-mail-open"></i>&nbsp; <a href="mailto:me@abarchibody.com">me@abarchibody.com</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--END SOCIAL-->
+</template>
+
+<script>
+export default {
+  name: "Social",
+  props: {}
+};
+</script>
